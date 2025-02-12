239d32b2c39869379ca40ded9db301c769a14106
PhucQuangg <nguyenhoangphuc020803@gmail.com>
2025-02-12 13:55:28 +0700

    First commit

---
+++ cinema-web/src/main/java/stu/edu/vn/cinema_web/CinemaWebApplication.java
@@ -0,0 +1,13 @@
+package stu.edu.vn.cinema_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaWebApplication.class, args);
+	}
+
+}
---
+++ cinema-web/src/test/java/stu/edu/vn/cinema_web/CinemaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package stu.edu.vn.cinema_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
